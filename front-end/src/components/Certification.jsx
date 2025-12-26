8d710f7ab0303c8dbc2e12bfa7eeba0985e72de5
devanadindraa <devanadindra.p@gmail.com>
2025-12-27 02:03:28 +0700

    certif

--- front-end/src/components/Certification.jsx
+++ front-end/src/components/Certification.jsx
@@ -60,72 +60,67 @@ export function Certification() {
     );
   }
 
-return (
-  <section ref={sectionRef} className="min-h-screen">
-    <CertifWaveUP />
-    <div className="pb-10 bg-[#8c2b7a]">
-      <div>
-        <div className="flex flex-wrap">
-          <div className="w-full">
-            <h4
-              className="font-bold uppercase text-white text-xl text-center md:text-2xl lg:text-4xl"
-              data-aos="fade-down"
-              data-aos-delay="500"
-              data-aos-duration="800"
-              data-aos-offset="500"
-            >
-              Certification
-            </h4>
-            {certifData && certifData.length > 0 && (
-              <>
-                <ReactSwipe
-                  key={certifData.length}
-                  className="carousel"
-                  ref={reactSwipeEl}
-                >
-                  {certifData?.map((certificate, index) => (
-                    <div className="flex justify-center pt-10" key={index}>
-                      {/* UKURAN GAMBAR: diubah dari w-[100%] ke w-[85%] agar lebih kecil di mobile */}
-                      <div
-                        className="w-[85%] sm:w-[70%] md:w-[70%] lg:w-[50%]" 
-                        data-aos="zoom-in"
-                        data-aos-delay="500"
-                        data-aos-duration="800"
-                        data-aos-offset="300"
-                      >
-                        {certificate.certif_link ? (
-                          <a
-                            href={certificate.certif_link}
-                            target="_blank"
-                            rel="noreferrer"
-                          >
+  return (
+    <section ref={sectionRef} className="min-h-screen">
+      <CertifWaveUP />
+      <div className="pb-10 bg-[#8c2b7a]">
+        <div>
+          <div className="flex flex-wrap">
+            <div className="w-full">
+              <h4
+                className="font-bold uppercase text-white text-xl text-center md:text-2xl lg:text-4xl"
+                data-aos="fade-down"
+                data-aos-delay="500"
+                data-aos-duration="800"
+                data-aos-offset="500"
+              >
+                Certification
+              </h4>
+              {certifData && certifData.length > 0 && (
+                <div className="relative">
+                  <ReactSwipe
+                    key={certifData.length}
+                    className="carousel"
+                    ref={reactSwipeEl}
+                  >
+                    {certifData?.map((certificate, index) => (
+                      <div className="flex justify-center pt-10" key={index}>
+                        {/* UKURAN GAMBAR: diubah dari w-[100%] ke w-[85%] agar lebih kecil di mobile */}
+                        <div
+                          className="w-[85%] sm:w-[70%] md:w-[70%] lg:w-[50%]"
+                          data-aos="zoom-in"
+                          data-aos-delay="500"
+                          data-aos-duration="800"
+                          data-aos-offset="300"
+                        >
+                          {certificate.certif_link ? (
+                            <a
+                              href={certificate.certif_link}
+                              target="_blank"
+                              rel="noreferrer"
+                            >
+                              <img
+                                className="certif_img w-full rounded-2xl bg-contain cursor-pointer hover:scale-105 transition-transform duration-300"
+                                src={`${API_BASE}${certificate.img_url}`}
+                                alt={certificate.name}
+                              />
+                            </a>
+                          ) : (
                             <img
-                              className="certif_img w-full rounded-2xl bg-contain cursor-pointer hover:scale-105 transition-transform duration-300"
+                              className="w-full rounded-2xl bg-contain cursor-default"
                               src={`${API_BASE}${certificate.img_url}`}
                               alt={certificate.name}
                             />
-                          </a>
-                        ) : (
-                          <img
-                            className="w-full rounded-2xl bg-contain cursor-default"
-                            src={`${API_BASE}${certificate.img_url}`}
-                            alt={certificate.name}
-                          />
-                        )}
+                          )}
+                        </div>
                       </div>
-                    </div>
-                  ))}
-                </ReactSwipe>
+                    ))}
+                  </ReactSwipe>
 
-                {/* CONTAINER NAVIGASI: Menggunakan flex untuk mobile, absolute untuk desktop */}
-                <div className="flex justify-center gap-10 mt-8 md:mt-0 md:static">
                   {/* Panah Kiri */}
                   <div
                     onClick={() => reactSwipeEl.current.prev()}
-                    className="cursor-pointer w-10 h-10 lg:w-14 lg:h-14 md:absolute md:left-5 md:top-1/2 md:-translate-y-1/2 lg:left-10"
-                    data-aos="fade-right"
-                    data-aos-delay="800"
-                    data-aos-duration="800"
+                    className="hidden md:flex absolute left-5 top-1/2 -translate-y-1/2 cursor-pointer w-14 h-14"
                   >
                     <FaArrowLeft className="text-white w-full h-full hover:scale-110 transition-transform" />
                   </div>
@@ -133,21 +128,17 @@ return (
                   {/* Panah Kanan */}
                   <div
                     onClick={() => reactSwipeEl.current.next()}
-                    className="cursor-pointer w-10 h-10 lg:w-14 lg:h-14 md:absolute md:right-5 md:top-1/2 md:-translate-y-1/2 lg:right-10"
-                    data-aos="fade-left"
-                    data-aos-delay="800"
-                    data-aos-duration="1000"
+                    className="hidden md:flex absolute right-5 top-1/2 -translate-y-1/2 cursor-pointer w-14 h-14"
                   >
                     <FaArrowRight className="text-white w-full h-full hover:scale-110 transition-transform" />
                   </div>
                 </div>
-              </>
-            )}
+              )}
+            </div>
           </div>
         </div>
       </div>
-    </div>
-    <ProjectWaveUP />
-  </section>
-);
+      <ProjectWaveUP />
+    </section>
+  );
 }
